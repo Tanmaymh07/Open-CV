c0603fb097189423453cfe8c129a332863a1dc87
Tanmay Mhatre <tanmaymhatre9033@gmail.com>
2022-01-16 14:47:53 +0530

    ho

--- Chapter4.py
+++ Chapter4.py
@@ -2,9 +2,16 @@ import cv2
 import numpy as np
 
 img = np.zeros((512,512,3),np.uint8)
-print(img)
-img[:]= 50,0,50
+# print(img)
+# img[:]= 255,0,50
 
+# cv2.line(img,(256,0),(400,300),(0,255,0),3)
+# cv2.line(img,(256,0),(100,300),(0,255,0),3)
+# cv2.line(img,(0,123),(512,123),(0,255,0),3)
+
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
+cv2.circle(img,(400,50),30,(255,255,0),5)
+cv2.putText(img," OPENCV ",(300,200),cv2.FONT_HERSHEY_COMPLEX,2,(0,150,0),1)
 cv2.imshow("Image",img)
 
 
